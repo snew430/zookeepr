e57cf1219868730bacf7062a7101b333a6719b88
snew430 <seanpnew@hotmail.com>
2022-02-20 14:30:18 -0600

    added validation for post

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const { animals } = require("./data/animals");
+const fs = require("fs");
+const path = require("path");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
-  // Note that we save the animalsArray as filteredResults here:
   let filteredResults = animalsArray;
   if (query.personalityTraits) {
     if (typeof query.personalityTraits === "string") {
@@ -44,6 +45,32 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   console.log(req.query);
@@ -63,8 +90,13 @@ app.get("/api/animals/:id", (req, res) => {
 });
 
 app.post("/api/animals", (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+  req.body.id = animals.length.toString();
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
